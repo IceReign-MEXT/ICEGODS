fca0771692efe4db52b26e7e31e127e925ceeca3
IceAlphaDev <your_email@gmail.com>
2025-12-23 01:34:48 +0100

    Finalize Analytics and SEO integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>IceGods Mainframe | $ICEG Token</title>
-        <meta name="google-site-verification" content="${VERIFICATION_CODE}" />
-        <meta name="description" content="The official automated hosting gateway for the $ICEG Empire. Mint your mainframe on Base." />
+        {/* Google Search Console Verification */}
+        <meta name="google-site-verification" content="PASTE_YOUR_CODE_STRING_HERE" />
+        <meta name="description" content="Official automated hosting gateway for the $ICEG Empire. Mint your mainframe on Base." />
       </Head>
+      
       <Component {...pageProps} />
+      
+      {/* Vercel Web Analytics */}
+      <Analytics />
     </>
   )
 }
